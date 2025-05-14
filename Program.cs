f34225a23dcf55070a64ce74b19b18097a66b7d1
Wodkapowa21 <rus.zerocool@gmail.com>
2025-05-14 13:22:04 +0200

    add swagger https://localhost:7001/swagger/index.html

--- Program.cs
+++ Program.cs
@@ -1,26 +1,29 @@
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Vehicle Price Calculator API",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vehicle Price Calculator API v1");
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.MapControllers();
-
 app.Run();
