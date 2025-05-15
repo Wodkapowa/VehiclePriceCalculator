2663b06e901a900f9dd8fe126bcb6e915f39562b
Primoz <rus.zerocool@gmail.com>
2025-05-15 13:50:47 +0200

    add default values

--- Models/VehiclePriceRequest.cs
+++ Models/VehiclePriceRequest.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel;
+
 namespace VehiclePriceCalculator.Models;
 
 public class VehiclePriceRequest
 {
+    [DefaultValue(10000)]
     public decimal? BaseNet { get; set; }
     public decimal? BaseGross { get; set; }
     public decimal? EquipmentNet { get; set; }
+    [DefaultValue(122)]
     public decimal? EquipmentGross { get; set; }
+    [DefaultValue(22)]
     public decimal VatRate { get; set; } // npr. 22%
 }
